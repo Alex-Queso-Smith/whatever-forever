ff21979d69c78e5e0f54c7759e96503b685ff1bb
Alex Smith <queso@Alexs-Air.fios-router.home>
2018-11-27 16:21:47 -0500

    foundation mild sucess

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require jquery_ujs
+//= require foundation
 //= require activestorage
 //= require_tree .
+
+$(function(){ $(document).foundation(); });
