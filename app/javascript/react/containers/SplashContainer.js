ba83170809247bf9acec0346c86f6a7289a72a02
Alex Smith <queso@Alexs-Air.fios-router.home>
2018-11-29 19:47:10 -0500

    background images

---
+++ app/javascript/react/containers/SplashContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class SplashContainer extends React.Component {
+  state = {}
+
+  render(){
+    return(
+      <div id="splash-container" >
+      
+      </div>
+    )
+  }
+}
+
+export default SplashContainer
