3c832830212858fd21e299a06f9919b6430d5250
Alex Smith <queso@Alexs-Air.fios-router.home>
2018-12-01 16:51:20 -0500

    fae setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  namespace :admin do
+  end
+  # mount Fae below your admin namespec
+  mount Fae::Engine => '/admin'
+
   root 'splashes#index'
 
   resources :splashes, only: [ :index ]
